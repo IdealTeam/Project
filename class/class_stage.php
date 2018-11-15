1646b5b30059970f7c32347eb917a5b1e5a154e6
Quentun <44780026+Quentun@users.noreply.github.com>
2018-11-15 08:21:26 +0100

    Add files via upload

--- class/class_stage.php
+++ class/class_stage.php
@@ -1,17 +1,19 @@
 <?php
-
-class stage
+include('class_offre.php');
+class stage EXTENDS offre
 {
     private $commentaire_stage;
     private $note_stage;
     
-    public function stage($commentairestage, $notestage) //constructeur
+    public function stage($idoffre,$libelleoffre,$datepublicationoffre,$datedebutoffre,$datefinoffre,$commentairestage,$notestage)
     {
+        offre::offre($idoffre,$libelleoffre,$datepublicationoffre,$datedebutoffre,$datefinoffre);
         $this->commentaire_stage = $commentairestage;
-        $this->note_stage  = $notestage;
+        $this->note_stage = $notestage;
        
     }
     
+    //GETTER OFFRE
     
     public function get_commentaire_stage()
     {
@@ -22,6 +24,8 @@ class stage
     {
         return $this->notestage;
     }
+    
+    //SETTER OFFRE
     
     public function set_commentaire_stage($commentairestage)
     {
@@ -34,5 +38,8 @@ class stage
     }
  
 }
+
+$heritage2 = new stage(1,'ll','2222','22222','22222','jjgghgh',5);
+var_dump($heritage2);
 
 ?>
