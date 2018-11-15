660ba35125b25bc4738614d7c44adfcea5463189
Vds123 <valentin.dls@outlook.fr>
2018-11-15 10:13:11 +0100

    correction bug espace

--- class/class_offre.php
+++ class/class_offre.php
@@ -7,18 +7,18 @@ class offre
     private $date_publication_offre;
     private $date_debut_offre;
     private $date_fin_offre;
-    
-    public function offre($idoffre,$libelleoffre,$datepublicationoffre,$datedebutoffre,$datefinoffre)     
+
+    public function offre($idoffre,$libelleoffre,$datepublicationoffre,$datedebutoffre,$datefinoffre)
     {
         $this->id_offre = $idoffre;
-        $this->libelle_offre = $libelleoffre;
-        $this->date_publication_offre = $datepublicationoffre;
-        $this->date_debut_offre = $datedebutoffre;
+        $this->libelle_offre = $libelleoffre;
+        $this->date_publication_offre = $datepublicationoffre;
+        $this->date_debut_offre = $datedebutoffre;
         $this->date_fin_offre = $datefinoffre;
     }
-    
+
     //GETTER OFFRE
-    
+
     public function get_id_offre()
     {
         return $this->id_offre;
@@ -27,39 +27,39 @@ class offre
     {
         return $this->libelle_offre;
     }
-    
+
     public function get_date_publication_offre()
     {
         return $this->date_publication_offre;
     }
-    
+
     public function get_date_debut_offre()
     {
         return $this->date_debut_offre;
     }
-    
+
     public function get_date_fin_offre()
     {
         return $this->date_fin_offre;
     }
-    
+
     //SETTER OFFRE
-    
+
     public function set_libelle_offre($libelle)
     {
         $this->libelle_offre = $libelle;
     }
-    
+
     public function set_date_publication_offre($datepublicationoffre)
     {
         $this->date_publication_offre = $datepublicationoffre;
     }
-    
+
     public function set_date_debut_offre($datedebutoffre)
     {
-        $this->date_debut_offre  = $datedebutoffre;
+        $this->date_debut_offreï¿½ = $datedebutoffre;
     }
-    
+
     public function set_date_fin_offre($datefinoffre)
     {
         $this->date_fin_offre = $datefinoffre;
@@ -69,4 +69,3 @@ class offre
 $offre = new offre(4,'hgghgg','55415','5555','515155');
 var_dump($offre);
 ?>
-
