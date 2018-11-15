8977b0ed14636a28ff6219bc08513db03eabc5a9
Vds123 <valentin.dls@outlook.fr>
2018-11-15 16:54:14 +0100

    requete sql entreprise

--- class/class_entreprise.php
+++ class/class_entreprise.php
@@ -4,36 +4,42 @@ class entreprise EXTENDS user
 {
     private $raison_sociale_entreprise;
     private $contact_entreprise;
-    
+
     public function entreprise($raisonsocialeentreprise,$contactentreprise,$iduser,$nomuser,$teluser,$emailuser,$photouser,$loginuser,$pwuser,$etatuser)
     {
         user::user($iduser,$nomuser,$teluser,$emailuser,$photouser,$loginuser,$pwuser,$etatuser);
         $this->raison_sociale_entreprise = $raisonsocialeentreprise;
         $this->contact_entreprise = $contactentreprise;
     }
-    
+
     //GETTER ENTREPRISE
-    
+
     public function get_raison_sociale_entreprise()
     {
         return $this->raison_sociale_entreprise;
     }
-    
+
     public function get_contact_entreprise()
     {
         return $this->contact_entreprise;
     }
-    
-    //SETTER ENTREPRISE 
-    
+
+    //SETTER ENTREPRISE
+
     public function set_raison_sociale_entreprise($raisonsocialeentreprise)
     {
         $this->raison_sociale_entreprise = $raisonsocialeentreprise;
     }
-    
+
     public function set_raison_sociale_entreprise($contactentreprise)
     {
         $this->contact_entreprise = $contactentreprise;
+    }
+
+    public function ajout_entreprise($iduser, $nomuser, $teluser, $emailuser, $photouser, $loginuser, $pwuser, $etatuse)
+    {
+        $sql = "INSERT INTO entreprise values ('$raisonsocialeentreprise', '$contactentreprise', '$iduser', ''$nomuser', '$teluser', '$emailuser', '$photouser', '$loginuser', '$pwuser', '$etatuser')";
+        $conn -> Query($sql);
     }
 }
 
