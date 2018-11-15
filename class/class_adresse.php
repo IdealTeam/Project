1f1e6b3d23d35962fc6d7d416c3df45453b9851e
Vds123 <valentin.dls@outlook.fr>
2018-11-15 08:43:34 +0100

    essai avec Quentin

--- class/class_adresse.php
+++ class/class_adresse.php
@@ -5,28 +5,28 @@ class adresse
 {
     private $id_adresse;
     private $rue_adresse;
-    
-    
+
+
     public function adresse($idadresse,$rueadresse)
     {
         $this-> id_adresse = $rueadresse;
         $this-> rue_adresse = $idadresse;
     }
-    
+
     //GETTER ADRESSE
-    
+
     public function get_id_adresse()
     {
         return $this-> id_adresse;
     }
-    
+
     public function get_rue_adresse()
     {
         return $this-> rue_adresse;
     }
-    
+
     //SETTER ADRESSE
-    
+
     public function set_rue_adresse($rueadresse)
     {
         $this-> rue_adresse = $rueadresse;
