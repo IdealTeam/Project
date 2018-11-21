fa0747520d0fc9e7d50bbddcbe212aec327aee9c
Quentun <qfab11@hotmail.com>
2018-11-21 11:25:29 +0100

    include session

--- sessioncondition.inc.php
+++ sessioncondition.inc.php
@@ -1,12 +1,22 @@
 <?php session_start();
 if (!isset($_SESSION['IDENTIFIANT']))
 {
-   header ("Location:index.php");
+   // header ("Location:index.php");
+?>
+   <script type="text/javascript">
+     document.location.href="index.php";
+   </script>
+<?php
    exit();
 }
 if (isset($_GET['logout']))
 {
 	session_unset();
-	header("Location:index.php");
+  ?>
+     <script type="text/javascript">
+       document.location.href="index.php";
+     </script>
+  <?php
+	// header("Location:index.php");
 }
 ?>
