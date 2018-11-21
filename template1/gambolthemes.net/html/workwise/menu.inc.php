fa070a9b14c80936e0c5ef7f5eeaed190234d1d3
bns123 <hajar.pro@hotmail.com>
2018-11-21 11:49:16 +0100

    bfb

--- template1/gambolthemes.net/html/workwise/menu.inc.php
+++ template1/gambolthemes.net/html/workwise/menu.inc.php
@@ -197,7 +197,7 @@
             <li><a href="#" title="">Faqs</a></li>
             <li><a href="#" title="">Termes & Conditions</a></li>
           </ul>
-          <h3 class="tc"><a href="sign-in.php" title="">Déconnexion</a></h3>
+          <h3 class="tc"><a href="index.php?logout" title="">Déconnexion</a></h3>
         </div><!--user-account-settingss end-->
       </div>
     </div><!--header-data end-->
