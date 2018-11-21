06a67159d7357ed9588e6402ecc3f40f564adc60
Vds123 <valentin.dls@outlook.fr>
2018-11-21 10:33:10 +0100

    mise en place index

--- template1/gambolthemes.net/html/workwise/sessioncondition.inc.php
+++ template1/gambolthemes.net/html/workwise/sessioncondition.inc.php
@@ -1,12 +1,12 @@
 <?php session_start();
 if (!isset($_SESSION['IDENTIFIANT']))
 {
-   header ("Location:index.php");
+   header ("Location:index-2.php");
    exit();
 }
 if (isset($_GET['logout']))
 {
 	session_unset();
-	header("Location:index.php");
+	header("Location:index-2.php");
 }
 ?>
