38fa893f14ccf9699b55dd41e674112983fa61e4
Quentun <qfab11@hotmail.com>
2018-12-05 11:39:06 +0100

    Affichage des entreprises dans entreprises

--- template1/gambolthemes.net/html/workwise/requete_stage_emploi.php
+++ template1/gambolthemes.net/html/workwise/requete_stage_emploi.php
@@ -1,5 +1,5 @@
 <?php
-  //AFICHAGE DE TOUT LES STAGES
+  //AFICHAGE DE TOUT LES STAGES DANS STAGES
 	$stage = new stage('','','','','','','','');
   $sql_stage = "SELECT titre_offre,libelle_offre,date_publication_offre,photo_profil_user,nom_user,prenom_utilisateur,nom_commune
 	FROM offre,user,commune
@@ -8,6 +8,17 @@
   AND offre.etat_offre = 1
   AND offre.type_offre = 's'
   ORDER BY offre.date_publication_offre DESC;";
-  $req_stage = $stage->sql_offre($sql_stage,$conn) or die("erreur requete_stage_emploi".$sql_stage);
+  $req_stage = $stage->sql_stage($sql_stage,$conn) or die("erreur requete_stage_emploi".$sql_stage);
+
+  //AFICHAGE DE TOUT LES EMPLOIS DANS EMPLOIS
+	$offre = new offre('','','','','','');
+  $sql_offre = "SELECT titre_offre,libelle_offre,date_publication_offre,photo_profil_user,nom_user,prenom_utilisateur,nom_commune
+	FROM offre,user,commune
+	WHERE offre.id_user = user.id_user
+  AND offre.id_commune = commune.code_commune_INSEE
+  AND offre.etat_offre = 1
+  AND offre.type_offre = 's'
+  ORDER BY offre.date_publication_offre DESC;";
+  $req_offre = $offre->sql_offre($sql_offre,$conn) or die("erreur requete_stage_emploi".$sql_stage);
 
 ?>
