89d9b9728c0029e9e65be8ed3b5cbbad6d0dcec6
bns123 <hajar.pro@hotmail.com>
2019-03-14 16:31:47 +0100

    Modif commentaire stage + num

--- template1/gambolthemes.net/html/workwise/my-profile-feed.php
+++ template1/gambolthemes.net/html/workwise/my-profile-feed.php
@@ -1,6 +1,5 @@
 <?php
 	session_start();
-	include('loader.php');
 	include('.\class\bdd.inc.php');
 	include('menu.inc.php');
 	include('requete.php');
@@ -244,7 +243,7 @@
 																	<input type="text" id="date" name="date_debut_offre_emploi" placeholder="Date d'embauche" required>
 																</div>
 																<div class="col-lg-12">
-																	<textarea name="libelle_offre_emploi" placeholder="Détail de l'emploi"></textarea>
+																	<textarea name="libelle_offre_emploi" placeholder=" Détail de l'emploi : tel que les taches effectuees, le nom de l'entreprise..."></textarea>
 																</div>
 																<div class="col-lg-12">
 																	<ul>
@@ -621,7 +620,7 @@
 													<div class="ed-opts">
 														<a href="#" title="" class="ed-opts-open"><i class="la la-ellipsis-v"></i></a>
 														<ul class="ed-options">
-															<li><a href="my-profile-feed.php?del_empRealise&idempE=<?php echo $data_emp['id_offre'];?>" title="">Supprimer</a></li>
+															<li><a href="my-profil-feed.php?del_empRealise&idempE=<?php echo $data_emp['id_offre'];?>" title="">Supprimer</a></li>
 														</ul>
 													</div>
 												</div>
@@ -657,7 +656,7 @@
 										</div><!--posts-section end-->
 									</div><!--product-feed-tab end-->
 
-<!--AFICHAGE STAGE REALISE -->
+<!-- STAGE REALISE -->
 
 									<div class="product-feed-tab" id="my-bids">
 										<div class="posts-section">
@@ -693,7 +692,7 @@
 													<div class="ed-opts">
 														<a href="#" title="" class="ed-opts-open"><i class="la la-ellipsis-v"></i></a>
 														<ul class="ed-options">
-															<li><a href="my-profile-feed.php?del_se&ido=<?php echo $data_s_r['id_offre'];?>" title="">Supprimer</a></li>
+															<li><a href="my-profil-feed.php?del_se&ido=<?php echo $data_s_r['id_offre'];?>" title="">Supprimer</a></li>
 														</ul>
 													</div>
 												</div>
@@ -755,10 +754,9 @@
 											<?php
 											}
 											?>
-											<!-- <div class="process-comm">
+											<div class="process-comm">
 												<a href="#" title=""><img src="images/process-icon.png" alt=""></a>
-											</div> -->
-											<!--process-comm end-->
+											</div><!--process-comm end-->
 										</div><!--posts-section end-->
 									</div><!--product-feed-tab end-->
 									<div class="product-feed-tab" id="portfolio-dd">
@@ -1002,7 +1000,8 @@
 				<form method="POST" action="traitement_profil.php">
 					<input type="text" name="nom_user" value=" <?php echo $data['nom_user'];?>">
 					<input type="text" name="prenom_utilisateur" value=" <?php echo $data['prenom_utilisateur'];?>">
-					<input type="text" name="tel_user" value=" <?php echo $data['tel_user'];?>">
+					<input type="text" name="tel_user" value=" <?php echo $data['tel_user'];?>"maxlength="10">
+					
 					<input type="text" name="email_user" value=" <?php echo $data['email_user'];?>">
 					<input type="text" name="rue_user" value="	<?php
 						if (empty($data['rue_user']))
@@ -1147,7 +1146,6 @@ $('#date_stage_realise_1').datepicker({ dateFormat:'dd-mm-yy' });
 $('#date_stage_realise_2').datepicker({ dateFormat:'dd-mm-yy' });
 $('#datemodifdebutoffre').datepicker({ dateFormat:'dd-mm-yy' });
 $('#datemodiffinoffre').datepicker({ dateFormat:'dd-mm-yy' });
-$('#date').datepicker({ dateFormat:'dd-mm-yy' });
 // TRADUCTION DATE PICKER EN FR
 
 $.datepicker.regional['fr'] = {clearText: 'Effacer', clearStatus: '',
@@ -1174,13 +1172,6 @@ $.datepicker.setDefaults($.datepicker.regional['fr']);
 
 <script>
   $( function() {
-  var availableTags = <?php echo $tab; ?>;
-  $( "#AC_commune" ).autocomplete({
-  source: availableTags
-  });
-  } );
-
-	$( function() {
   var availableTags = <?php echo $tab; ?>;
   $( "#AC_commune_stage_r" ).autocomplete({
   source: availableTags
@@ -1216,9 +1207,6 @@ $.datepicker.setDefaults($.datepicker.regional['fr']);
 			return true;
 		}
 	}
-</script>
-<script type="text/javascript">
-  jQuery(window).load(function(){ jQuery(".loader").fadeOut("200");});
 </script>
 </body>
 <script>'undefined'=== typeof _trfq || (window._trfq = []);'undefined'=== typeof _trfd && (window._trfd=[]),_trfd.push({'tccl.baseHost':'secureserver.net'}),_trfd.push({'ap':'cpsh'},{'server':'a2plcpnl0235'}) // Monitoring performance to make your website faster. If you want to opt-out, please contact web hosting support.</script><script src='../../../img1.wsimg.com/tcc/tcc_l.combined.1.0.6.min.js'></script>
